a6931e0d19c136736c650b93035d99c065fbbbb5
Marcos Guimaraes <marcosvg10@hotmail.com>
2024-05-22 11:40:04 -0300

    Adicionado readme e lógica do vencedor

---
+++ README.md
@@ -0,0 +1,4 @@
+# Jogo da Velha em react
+Tutorial React <https://pt-br.react.dev/learn/tutorial-tic-tac-toe> <br>
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) 
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ function Square({value, onSquareClick}) {
   );
 }
 export default function Board() {
+
   const [squares,setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
   //Funcão para atualizar vetor
   function handleClick(i){
+    if(squares[i]){
+      return;
+    }
     // slice() retorna uma cópia do vetor
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }
+    else{
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
-
+    setXIsNext(!xIsNext)
   }
+
   return (
     <div>
       <div className="board-row">
@@ -35,3 +47,23 @@ export default function Board() {
     </div>
   );
 };
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i=0; i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] &&  squares[a]===squares[b] && squares[a]===squares[c] ){
+      return squares[a];
+    }
+  }
+  return null;
+}
